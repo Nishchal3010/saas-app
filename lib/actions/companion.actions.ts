dfcf4a25c8a5705f03456fd506efa1016def46ba
Nishchal <nishchalpurohit7@gmail.com>
2026-07-28 13:54:56 +0530

    feat: add individual companion session page with getCompanion action

--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -1,46 +1,62 @@
-'use server'
+"use server";
 
-import { auth } from "@clerk/nextjs/server"
-import { createSupabaseClient } from "@/lib/supabase"
+import { auth } from "@clerk/nextjs/server";
+import { createSupabaseClient } from "@/lib/supabase";
 
 export const createCompanion = async (formData: CreateCompanion) => {
-  const { userId: author } = await auth()
-  const supabase = await createSupabaseClient()
+  const { userId: author } = await auth();
+  const supabase = await createSupabaseClient();
 
   const { data, error } = await supabase
-    .from('companions')
+    .from("companions")
     .insert({ ...formData, author })
-    .select()
+    .select();
 
   if (error || !data) {
-    throw new Error(error?.message || "Failed to create companion")
+    throw new Error(error?.message || "Failed to create a companion");
   }
 
-  return data[0]
-}
+  return data[0];
+};
 
-export const getAllCompanions = async ({ limit = 10, page = 1, subject, topic }) => {
-  const supabase = await createSupabaseClient()
+export const getAllCompanions = async ({
+  limit = 10,
+  page = 1,
+  subject,
+  topic,
+}: GetAllCompanions) => {
+  const supabase = await createSupabaseClient();
 
-  let query = supabase
-    .from('companions')
-    .select()
+  let query = supabase.from("companions").select();
 
   if (subject && topic) {
     query = query
-      .ilike('subject', `%${subject}%`)
-      .or(`topic.ilike.%${topic}%,name.ilike.%${topic}%`)
+      .ilike("subject", `%${subject}%`)
+      .or(`topic.ilike.%${topic}%,name.ilike.%${topic}%`);
   } else if (subject) {
-    query = query.ilike('subject', `%${subject}%`)
+    query = query.ilike("subject", `%${subject}%`);
   } else if (topic) {
-    query = query.or(`topic.ilike.%${topic}%,name.ilike.%${topic}%`)
+    query = query.or(`topic.ilike.%${topic}%,name.ilike.%${topic}%`);
   }
 
-  query = query.range((page - 1) * limit, page * limit - 1)
+  query = query.range((page - 1) * limit, page * limit - 1);
 
-  const { data: companions, error } = await query
+  const { data: companions, error } = await query;
 
-  if (error) throw new Error(error.message)
+  if (error) throw new Error(error.message);
 
   return companions;
-}
+};
+
+export const getCompanion = async (id: string) => {
+  const supabase = await createSupabaseClient();
+
+  const { data, error } = await supabase
+    .from("companions")
+    .select()
+    .eq("id", id);
+
+  if (error) return console.log(error);
+
+  return data[0];
+};
