39530fe764de052a98be6a3f596b67f87b007bc2
Nishchal <nishchalpurohit7@gmail.com>
2026-08-03 17:46:03 +0530

    feat: add Lottie soundwave animation and user avatar section to CompanionComponent

--- components/CompanionComponent.tsx
+++ components/CompanionComponent.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-"use client";
-
-import { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef, use } from "react";
+import Image from "next/image";
 import { cn, getSubjectColor } from "@/lib/utils";
 import { vapi } from "@/lib/vapi.sdk";
-import Image from "next/image";
+import Lottie from "lottie-react";
+import soundwaves from "@/constants/soundwaves.json";
 
 enum CallStatus {
   INACTIVE = "INACTIVE",
@@ -38,6 +38,15 @@ const CompanionComponent = ({
   const [callStatus, setCallStatus] = useState<CallStatus>(CallStatus.INACTIVE);
   const [isSpeaking, setIsSpeaking] = useState(false);
   const [isMuted, setIsMuted] = useState(false);
+
+	const lottieRef = useRef<LottieRefCurrentProps>(null);
+
+	useEffect(() =>{
+       if(lottieRef) {
+				if(isSpeaking) lottieRef.current?.play();
+				else lottieRef.current?.stop();
+			 }
+	}, [isSpeaking, lottieRef])
 
   useEffect(() => {
     const onCallStart = () => setCallStatus(CallStatus.ACTIVE);
@@ -77,24 +86,46 @@ const CompanionComponent = ({
             style={{ backgroundColor: getSubjectColor(subject) }}
           >
             <div
-  className={cn(
-    "absolute transition-opacity duration-1000",
-    callStatus === CallStatus.FINISHED || callStatus === CallStatus.INACTIVE
-      ? "opacity-100"
-      : "opacity-0",
-    callStatus === CallStatus.CONNECTING && "opacity-100 animate-pulse"
-  )}
->
-  <Image
-    src={`/icons/${subject}.svg`}
-    alt={subject}
-    width={150}
-    height={150}
-    className="max-sm:w-fit"
-  />
-</div>
+              className={cn(
+                "absolute transition-opacity duration-1000",
+                callStatus === CallStatus.FINISHED || callStatus === CallStatus.INACTIVE
+                  ? "opacity-100"
+                  : "opacity-0",
+                callStatus === CallStatus.CONNECTING && "opacity-100 animate-pulse"
+              )}
+            >
+              <Image
+                src={`/icons/${subject}.svg`}
+                alt={subject}
+                width={150}
+                height={150}
+                className="max-sm:w-fit"
+              />
+            </div>
+
+            <div className={cn('absolute transition-opacity duration-1000', callStatus === CallStatus.ACTIVE ? 'opacity-100': 'opacity-0')}> 
+                       <Lottie 
+											 lottieRef={lottieRef}
+											 animationData={soundwaves}
+											 autoplay={false}
+											 className="companion-lottie"
+											 />
+           </div>
           </div>
+          <p className="font-bold text-2xl">{name}</p>
         </div>
+
+				<div className="user-section">
+  <div className="user-avatar">
+    <Image
+      src={userImage}
+      alt={userName}
+      width={130}
+      height={130}
+      className="rounded-lg"
+    />
+  </div>
+</div>
       </section>
     </section>
   );
